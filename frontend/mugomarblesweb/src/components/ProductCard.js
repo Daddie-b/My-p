d85aea5140719ac4d250710d78e5b5ca4d902128
Daddie-b <billwahomebill@gmail.com>
2024-09-09 15:31:43 +0300

    product page updates

--- frontend/mugomarblesweb/src/components/ProductCard.js
+++ frontend/mugomarblesweb/src/components/ProductCard.js
@@ -1,19 +1,24 @@
-// src/components/ProductCard.js
 import React from 'react';
 import './ProductCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faMoneyBillWave } from '@fortawesome/free-solid-svg-icons';
 
 const ProductCard = ({ product, onAddToCart, onBuyNow }) => {
-  const { name, price, rating, image } = product;
+  const { name, price, description, image } = product;
 
   return (
     <div className="product-card">
       <img src={image} alt={name} className="product-image" />
       <h3 className="product-name">{name}</h3>
       <p className="product-price">Price: KSh {price}</p>
-      <p className="product-rating">Rating: {rating} / 5</p>
+      <p className="product-description">{description}</p>
       <div className="product-actions">
-        <button onClick={() => onAddToCart(product)}>Add to Cart</button>
-        <button onClick={() => onBuyNow(product)}>Buy Now</button>
+        <button onClick={() => onAddToCart(product)} className="icon-button">
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </button>
+        <button onClick={() => onBuyNow(product)} className="icon-button">
+          <FontAwesomeIcon icon={faMoneyBillWave} />
+        </button>
       </div>
     </div>
   );
