dc1d6373fd6ef2c8245249a220b26de6057bb14c
Daddie-b <billwahomebill@gmail.com>
2024-11-19 10:10:24 +0300

    server url updated

--- backend/server.js
+++ backend/server.js
@@ -62,12 +62,9 @@ app.delete('/api/products/:id', async (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB Connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 
 const PORT = process.env.PORT || 5000;
